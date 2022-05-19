a47bf27b741bb3a1689fb8d26d3107123199db2c
emiigomezm <70350212+emiigomezm@users.noreply.github.com>
2022-05-19 20:29:45 -0300

    Add files via upload

---
+++ compra.ts
@@ -0,0 +1,5 @@
+let compra: number = 450.5;
+let desc: number = 0.1;
+let final: number=compra-(compra*desc);
+//colocamos el resultado final
+console.log("la compra final con descuento es:"+final);
