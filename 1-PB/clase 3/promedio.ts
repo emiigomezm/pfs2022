e516c380e3dcd8277175694ec313159762dc61f6
emiigomezm <emilianogomez11@hotmail.com>
2022-05-30 20:00:47 -0300

    main

---
+++ 1-PB/clase 3/promedio.ts
@@ -0,0 +1,11 @@
+import * as rls from "readline-sync";
+let nota, suma, promedio, contador : number;
+contador =1;
+suma = 0
+while (contador<=10)[
+    nota= rls.questionInt("Ingrese una nota: "),
+    suma = suma + nota,
+    contador = contador +1,
+]
+promedio = suma/10
+console.log("el promedio de las notas es: " + promedio);
