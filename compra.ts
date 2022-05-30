460fa8cecad9eba3f99e2a3466f643533dcff9ba
emiigomezm <emilianogomez11@hotmail.com>
2022-05-30 20:01:19 -0300

    main

--- compra.ts
+++
@@ -1,5 +0,0 @@
-let compra: number = 450.5;
-let desc: number = 0.1;
-let final: number=compra-(compra*desc);
-//colocamos el resultado final
-console.log("la compra final con descuento es:"+final);
